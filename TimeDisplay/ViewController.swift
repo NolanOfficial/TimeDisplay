00a3461ddfd3c4eb5b2ac921627bb5d833c192a6
Nolan Fuchs <nolan1235@gmail.com>
2019-01-29 16:45:02 -0700

    -v 1.0.0 - 1st commit

--- TimeDisplay/ViewController.swift
+++ TimeDisplay/ViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dateAndTimeDownload: UILabel!
+    @IBOutlet weak var losAngelesDownload: UILabel!
+    @IBOutlet weak var chicagoDownload: UILabel!
+    @IBOutlet weak var newYorkDownload: UILabel!
+    
+    
+    var timer = Timer()
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        addTimer()
     }
 
+    func addTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.date), userInfo: nil, repeats: true)
+    }
+    
+    // Date & Time
+    @objc func date() {
+        
+        let currentDateTime = Date()
+        let losAngelesDateTime = currentDateTime.addingTimeInterval(-3600)
+        let chicagoDateTime = currentDateTime.addingTimeInterval(3600)
+        let newYorkDateTime = currentDateTime.addingTimeInterval(7200)
+        
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .medium
+        
+        dateAndTimeDownload.text = formatter.string(from: currentDateTime)
+         losAngelesDownload.text = formatter.string(from: losAngelesDateTime)
+        chicagoDownload.text = formatter.string(from: chicagoDateTime)
+         newYorkDownload.text = formatter.string(from: newYorkDateTime)
+        
+    }
+    
 
 }
 
