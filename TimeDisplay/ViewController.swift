f9e5b3451c14c0458387e6d93ea9fbc34b1d1c17
Nolan Fuchs <nolan1235@gmail.com>
2019-01-29 16:08:50 -0700

    Initial Commit

---
+++ TimeDisplay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimeDisplay
+//
+//  Created by Nolan Fuchs on 1/29/19.
+//  Copyright © 2019 Nolan Fuchs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
